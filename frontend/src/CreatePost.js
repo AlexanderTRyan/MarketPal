0837683b5acf8363aeac7bb215c6b926e9903dd9
Seth Proctor <sethproc@iastate.edu>
2024-04-17 10:36:19 -0500

    branch

--- frontend/src/CreatePost.js
+++ frontend/src/CreatePost.js
@@ -43,6 +43,7 @@ function CreatePost() {
       </div>
 
       <div>
+        <h1>Hello</h1>
         <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 titleInput"
           type="text" placeholder="Title" />
 

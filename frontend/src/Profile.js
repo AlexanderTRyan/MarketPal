46d8bd47709736b55e7bc2962b3888d0086d2771
Alexander Ryan <atryan@iastate.edu>
2024-04-18 10:39:02 -0500

    fixed a null error that caused a crash

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -1,14 +1,13 @@
 function Profile(userProfile) {
-    const profile = userProfile.userProfile;
-    return (
-      <div className="Profile">
-        <h1>Profile</h1>
-        <p>Name: {profile.fullName}</p>
-        <p>Email: {profile.email}</p>
-        <p>Address: {profile.address}</p>
+  const profile = userProfile.userProfile || {}; // If userProfile is null, assign an empty object
+  return (
+    <div className="Profile">
+      <h1>Profile</h1>
+      <p>Name: {profile.fullName || ''}</p>
+      <p>Email: {profile.email || ''}</p>
+      <p>Address: {profile.address || ''}</p>
+    </div>
+  );
+}
 
-      </div>
-    );
-  }
-  
-  export default Profile;
+export default Profile;

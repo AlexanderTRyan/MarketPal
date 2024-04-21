d234c503cc71b3f7b434b545dbbd7fd2a3b256e3
Seth Proctor <sethproc@iastate.edu>
2024-04-21 14:46:13 -0500

    worked on the browse screen

--- frontend/src/Browse.js
+++ frontend/src/Browse.js
@@ -1,85 +1,172 @@
+
 import React, { useState, useRef, useEffect } from 'react';
 
 function Browse() {
 
 
 
-
+  //Hook to track and unpdate the post Catalog. Holds all of the posts.
   const [postCatalog, setPostCatalog] = useState([]);
+
+  //Created for sorting purposes. We can't change postCatalog when sorting or we would destroy data. 
+  const [sortedPostCatalog, setSortedPostCatalog] = useState([]);
+
+  //Hook that determines whether to show the popup or not.
+  const [viewPostPopup, setViewPostPopup] = useState(false);
+
+  //Hook for holding the value of the post that was clicked on.
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  //Hook for the selected posts index.
+  const [selectedPostIndex, setSelectedPostIndex] = useState(null);
 
   //Using useEffect we are ensureing that the fetch is only being run 1 time 
   useEffect(() => {
     fetch("http://localhost:8081/listPosts")
       .then(response => response.json())
+      //imgIndex: 0 tells us the code to display img at index 0 by default
       .then(posts => {
         const postsWithIndex = posts.map(post => ({ ...post, imgIndex: 0 }));
         setPostCatalog(postsWithIndex);
+        setSortedPostCatalog(postsWithIndex);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
   }, []);
 
- 
+
+  //Closes the popup when the close button is clicked
+  const handleClosePopup = () => {
+    setSelectedPost(null);
+    setViewPostPopup(false);
+  }
+
+  //Displays the popup and sets the current post and the index. 
+  const handlePostClick = (post, index) => {
+    setSelectedPost(post);
+    setSelectedPostIndex(index);
+    setViewPostPopup(true);
+
+  }
+
+  const handleCategoryButtonClick = (category) => {
+    let results = [];
+    console.log("this is the cat:", category);
+    if (category != "All Listings") {
+      results = postCatalog.filter(allItems => {
+        return allItems.category.toLowerCase().includes(category.toLowerCase());
+      });
+    } else{
+      results = postCatalog;
+    }
+
+    console.log("these are the results", results);
+    return setSortedPostCatalog(results);
+
+  }
 
 
-  const listPosts = postCatalog.map((post, index) => {
+
+
+
+  //Shows the popup takes the parameters post and index which are the two hooks set at the top. 
+  function PostPopup({ post, index }) {
+
+    //This allwos the user to click the left arrow and flip through the images.
     const switchLeft = () => {
-      setPostCatalog(prevCatalog => {
+      setSortedPostCatalog(prevCatalog => {
+        //Copy of the postCatalog
         const updatedCatalog = [...prevCatalog];
+        //index is what post you are looking at in the postCatalog
+        //imgIndex is the index of the image array within the current post.
         if (updatedCatalog[index].imgIndex === 0) {
           updatedCatalog[index].imgIndex = updatedCatalog[index].imageUrl.length - 1;
         }
-        else{
+        else {
           updatedCatalog[index].imgIndex--;
         }
         return updatedCatalog;
       });
     };
-  
+
+
+    //Allows user to click through the photes by clicking the right button.
     const switchRight = () => {
-      setPostCatalog(prevCatalog => {
+      setSortedPostCatalog(prevCatalog => {
         const updatedCatalog = [...prevCatalog];
         if (updatedCatalog[index].imageUrl.length - 1 === updatedCatalog[index].imgIndex) {
-            updatedCatalog[index].imgIndex = 0;
+          updatedCatalog[index].imgIndex = 0;
         }
-        else{
+        else {
           updatedCatalog[index].imgIndex++;
         }
         return updatedCatalog;
       });
     };
 
+
+    //Returns the popup and the content from the post. 
     return (
-      <div className="col-4 mb-4" key={index}>
-        <div className="card shadow-sm">
-          <h3 className="title">{post.title}</h3>
+      <div className='popup-overlay'>
+        <div className='popup-content'>
+          <div className="preview-div">
+            <div>
+              <div className='img-preview-div-popup'>
+                {post.imageUrl && post.imageUrl.length > 0 && (
+                  <img className="bd-placeholder-img card-img-top max-width" height="225" src={post.imageUrl[post.imgIndex]} alt={post.title} />
+                )}
+                <div>
+                  <img src="https://img.icons8.com/?size=48&id=19175&format=png" alt="Left Arrow" onClick={switchLeft} className='img-arrow1' />
+                  <img src='https://img.icons8.com/?size=48&id=19175&format=png' alt="Right Arrow" onClick={switchRight} className='img-arrow2' />
+                </div>
+              </div>
+            </div>
+
+            <div className="preview-info">
+              <div className="preview-info-content">
+                <h2 className='preview-info-h1'>{post.title}</h2>
+                <h4 className='preview-info-h3'>${post.price}</h4>
+                <h3 className='preview-info-h3'>Condition:{post.condition}</h3>
+                <p className='preview-info-h3'>Description:{post.description}</p>
+                <hr />
+
+                <h3 className='preview-info-h3'>Seller Information:</h3>
+                <button onClick={handleClosePopup}>Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+
+
+  //This is the overall list of posts that are displayed on the browse screen.
+  const listPosts = sortedPostCatalog.map((post, index) => {
+
+    return (
+      <div className='posts-div' key={index} onClick={() => handlePostClick(post, index)}>
+        <div className="card">
+
           {post.imageUrl && post.imageUrl.length > 0 && (
             <img className="bd-placeholder-img card-img-top max-width" height="225" src={post.imageUrl[post.imgIndex]} alt={post.title} />
           )}
           <div className="card-body">
-            <div>
-              <img src="https://img.icons8.com/?size=48&id=9NUOGb9gL5Wb&format=gif" alt="Left Arrow" onClick={switchLeft} className='img-arrow1' />
-              <img src='https://img.icons8.com/?size=48&id=9NUOGb9gL5Wb&format=gif' alt="Right Arrow" onClick={switchRight} className='img-arrow2' />
-            </div>
-            <p className="card-text">{post.description}</p>
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="btn-group">
-                <p className="card-body">Price: {post.price.toLocaleString()}</p>
-                <p className="card-body">Condition: {post.condition}</p>
-                <p className="card-body">Category: {post.category}</p>
-              </div>
-            </div>
+            <p className="card-price">${parseFloat(post.price).toLocaleString()}</p>
+            <p className="title-post">{post.title}</p>
+            <p className="title-post">Condition: {post.condition}</p>
+
           </div>
         </div>
       </div>
     );
   });
 
-
+  //List of the categorys from users to easily filter through items on the market.
   return (
     <div className="Browse">
-
 
       <div className="left-section-browse">
         <div className="browse-title">
@@ -97,107 +184,112 @@ function Browse() {
         <div className='category-buttons-div'>
           <h3 className='category-buttons-header'>Category</h3>
           <hr />
-          <button className="category-buttons">
+
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("All Listings")}>
+            <img src="https://img.icons8.com/?size=50&id=78230&format=png" alt="Image1" />
+            All Listings
+          </button>
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Antiques & Collectibles")}>
             <img src="https://img.icons8.com/?size=80&id=RWBvrURKeNaa&format=png" alt="Image1" />
             Antiques & Collectibles
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Arts & Crafts")}>
             <img src="https://img.icons8.com/?size=80&id=111471&format=png" alt="Image2" />
             Arts & Crafts
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Auto Parts & Accessories")}>
             <img src="https://img.icons8.com/?size=50&id=24548&format=png" alt="Image3" />
             Auto Parts & Accessories
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Baby Products")}>
             <img src="https://img.icons8.com/ios-filled/2x/onesie.png" alt="Image4" />
             Baby Products
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Books, Movies & Music")}>
             <img src="https://img.icons8.com/?size=50&id=9480&format=png" alt="Image5" />
             Books, Movies & Music
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Cell Phones & Accessories")}>
             <img src="https://img.icons8.com/?size=50&id=yCeoujsiNAwK&format=png" alt="Image6" />
             Cell Phones & Accessories
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Clothing, Shoes, & Accessories")}>
             <img src="https://img.icons8.com/?size=64&id=TiPE0w163waq&format=png" alt="Image7" />
             Clothing, Shoes, & Accessories
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Electronics")}>
             <img src="https://img.icons8.com/?size=50&id=8576&format=png" alt="Image8" />
-            Electronincs
+            Electronics
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Furniture")}>
             <img src="https://img.icons8.com/?size=50&id=8034&format=png" alt="Image9" />
             Furniture
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Health & Beauty")}>
             <img src="https://img.icons8.com/?size=80&id=RpFbZUCbp8X6&format=png" alt="Image10" />
             Health & Beauty
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Home & Kitchen")}>
             <img src="https://img.icons8.com/?size=50&id=6qMXiF4hK09X&format=png" alt="Image11" />
             Home & Kitchen
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Jewelry & Watches")}>
             <img src="https://img.icons8.com/?size=80&id=jXfQurfp3MQM&format=png" alt="Image12" />
-            Jewerly & Watches
+            Jewelry & Watches
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Musical Instruments")}>
             <img src="https://img.icons8.com/?size=80&id=bts1JjTAeYAG&format=png" alt="Image13" />
             Musical Instruments
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Office Supplies")}>
             <img src="https://img.icons8.com/?size=50&id=mBUZsi8BBR6a&format=png" alt="Image14" />
             Office Supplies
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Pet Supplies")}>
             <img src="https://img.icons8.com/?size=80&id=121381&format=png" alt="Image15" />
             Pet Supplies
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Patio & Garden")}>
             <img src="https://img.icons8.com/?size=50&id=5I9mgW1o2sOm&format=png" alt="Image16" />
             Patio & Garden
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Sporting Goods")}>
             <img src="https://img.icons8.com/?size=80&id=CVJwB14NKRXA&format=png" alt="Image17" />
             Sporting Goods
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Tools & Home Improvement")}>
             <img src="https://img.icons8.com/?size=50&id=11219&format=png" alt="Image18" />
             Tools & Home Improvement
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Toys & Games")}>
             <img src="https://img.icons8.com/?size=80&id=fvMafQHrTSEc&format=png" alt="Image19" />
             Toys & Games
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Travel & Luggage")}>
             <img src="https://img.icons8.com/?size=50&id=pkoXh07CXQvX&format=png" alt="Image20" />
             Travel & Luggage
           </button>
 
-          <button className="category-buttons">
+          <button className="category-buttons" onClick={() => handleCategoryButtonClick("Miscellenous")}>
             <img src="https://img.icons8.com/?size=80&id=112518&format=png" alt="Image21" />
             Miscellenous
           </button>
@@ -211,6 +303,9 @@ function Browse() {
           {listPosts}
         </div>
       </div>
+      {/*This checks if the two hooks viewPostPopup and selectedPost are not null and are truthy. 
+      If the two hooks are truthy then the PostPopup function is called with the correct parameters.*/}
+      {viewPostPopup && selectedPost && <PostPopup post={selectedPost} index={selectedPostIndex} />}
     </div>
   );
 }
